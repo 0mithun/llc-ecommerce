2f3973ded56cb2615ca79bb5f6bb9e5420cc8c55
Mithun Halder <mithunrptc@gmail.com>
2019-01-22 22:43:10 +0600

    header & footer partial file created

---
+++ resources/views/frontend/partials/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+        <a href="#">Back to top</a>
+        </p>
+        <p>LLC Ecommerce is an ecommerce management platform develop using laravel.</p>
+    </div>
+</footer>
